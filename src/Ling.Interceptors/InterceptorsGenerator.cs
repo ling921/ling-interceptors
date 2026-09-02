ce7a85bb113f0621176de7f1fbb5116f508b5736
Jing Ling <lingjing0921@live.com>
2026-09-03 00:27:47 +0800

    Prepare v1.0.0 release

--- src/Ling.Interceptors/InterceptorsGenerator.cs
+++ src/Ling.Interceptors/InterceptorsGenerator.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.IO;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
--- tests/Ling.Interceptors.Tests/InterceptorsTests.cs
+++ tests/Ling.Interceptors.Tests/InterceptorsTests.cs
@@ -1,5 +1,4 @@
 using System.Collections.Immutable;
-using Ling.Interceptors;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Diagnostics;
